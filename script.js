a850682888b881e478502d4ca796033d4fdf5830
Pneuma <syca0714@gmail.com>
2020-11-17 19:01:22 +0900

    .

---
+++ script.js
@@ -0,0 +1,10 @@
+const socket = io()
+
+document.querySelector('#send').addEventListener('click', () => {
+    socket.emit('msgSend', { content: document.querySelector('#msg').value })
+    document.querySelector('#msg').value = ''
+})
+
+socket.on('msgReceive', data => {
+    document.querySelector('#chat').value += data + '\n'
+})
---
+++ server.js
@@ -0,0 +1,24 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', socket => {
+    console.log('A user connected')
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected')
+    })
+
+    socket.on('msgSend', data => {
+        console.log(data.content)
+        socket.emit('msgReceive', { content: data.content })
+    })
+})
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+})
